3352a4b2e770c579a3b207ab193db9929f3d9fde
SergioRD99 <sergio02.99ramos@gmail.com>
2024-04-13 11:50:30 -0600

    ca
    ca

--- GenerateQR/WinQR/Form1.cs
+++ GenerateQR/WinQR/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Imaging;
+
 namespace WinQR
 {
     public partial class Form1 : Form
@@ -5,6 +7,34 @@ namespace WinQR
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_generateQR_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw mGen =
+                Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            PBQR.Image = mGen.Draw(textBox1.Text, 100);
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.DefaultExt = ".png";
+            save.Filter = "Archivos PNG (*.png)|*.png";
+            save.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                string codigoQR = save.FileName;               
+                if (PBQR.Image != null)
+                {                 
+                    PBQR.Image.Save(codigoQR, ImageFormat.Png);
+                }
+                else
+                {
+                    MessageBox.Show("No hay una imagen para guardar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
         }
     }
 }
--- GenerateQR/WinQR/obj/Debug/net7.0-windows/WinQR.AssemblyInfo.cs
+++ GenerateQR/WinQR/obj/Debug/net7.0-windows/WinQR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinQR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42e54590dbd6b1efab59f7c3a2228e020fc56afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinQR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinQR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
